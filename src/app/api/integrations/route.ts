ff3ccbf1f4fe41649d1ddede4e8df58531d365f5
aw1875 <aw1875@rit.edu>
2023-07-08 20:30:35 -0500

    chore: added more integrations routes and a health check route

--- src/app/api/integrations/route.ts
+++ src/app/api/integrations/route.ts
@@ -50,3 +50,94 @@ export async function POST(request: NextRequest) {
     );
   }
 }
+
+export async function PUT(request: NextRequest) {
+  try {
+    const currentUser = await getAuthSession();
+
+    if (!currentUser)
+      return new NextResponse(JSON.stringify({ message: 'Unauthorized' }), { status: 401 });
+
+    const body = await request.json();
+    const { integrations, remove } = body;
+
+    if (!integrations || !integrations.length)
+      return new NextResponse(JSON.stringify({ message: 'No integrations provided' }), {
+        status: 400
+      });
+
+    await prisma.user.update({
+      where: { id: currentUser.user.id },
+      data: {
+        integrations: {
+          connect: integrations.map((integration: string) => ({ id: integration })),
+          disconnect: remove.map((integration: string) => ({ id: integration }))
+        }
+      }
+    });
+
+    return new NextResponse(
+      JSON.stringify({
+        message: 'Integrations successfully updated'
+      }),
+      {
+        status: 200
+      }
+    );
+  } catch (error) {
+    if (error instanceof Error)
+      return new NextResponse(JSON.stringify({ message: error.message }), {
+        status: 500
+      });
+
+    return new NextResponse(
+      JSON.stringify({ message: 'An error occured. Please try again later.' }),
+      {
+        status: 500
+      }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    const currentUser = await getAuthSession();
+
+    if (!currentUser)
+      return new NextResponse(JSON.stringify({ message: 'Unauthorized' }), { status: 401 });
+
+    const [userIntegrations, integrations] = await prisma.$transaction([
+      prisma.integrations.findMany({
+        where: {
+          userId: currentUser.user.id
+        },
+        select: {
+          id: true,
+          name: true,
+          image: true
+        }
+      }),
+      prisma.integrations.findMany({
+        select: {
+          id: true,
+          name: true,
+          image: true
+        }
+      })
+    ]);
+
+    return new NextResponse(JSON.stringify({ userIntegrations, integrations }), { status: 200 });
+  } catch (error) {
+    if (error instanceof Error)
+      return new NextResponse(JSON.stringify({ message: error.message }), {
+        status: 500
+      });
+
+    return new NextResponse(
+      JSON.stringify({ message: 'An error occured. Please try again later.' }),
+      {
+        status: 500
+      }
+    );
+  }
+}
